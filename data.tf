fb2a8b2ce823501fff92aa98d4e579821ae1da98
DuffMan2020 <57716152+DavidDTessier@users.noreply.github.com>
2022-01-28 08:26:15 -0500

    commented out data

--- data.tf
+++ data.tf
@@ -2,12 +2,12 @@ data "template_file" "user_data" {
   template = file("${abspath(path.module)}/userdata.yaml")
 }
 
-data "aws_vpc" "default_vpc" {
+/*data "aws_vpc" "default_vpc" {
   filter {
     name = "tag:Name"
     values = ["Default"]
   }
-}
+}*/
 
 data "aws_ami" "east-amazon-linux-2" {
   most_recent = true
